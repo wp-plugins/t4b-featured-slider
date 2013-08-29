f7a8ee9e630a4bf8bd675399c609c362e12f7ab6
moviehour <moviehour@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-29 14:46:26 +0000

    First version of T4B Featured Slider
    git-svn-id: https://plugins.svn.wordpress.org/t4b-featured-slider/trunk@764250 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ t4b_admin.php
@@ -0,0 +1,30 @@
+<?php
+add_action('admin_menu', 't4b_register_menu');
+define( 't4b_DOMAIN', 't4b-featured-slider' );
+
+function t4b_register_menu() {
+	add_menu_page('T4B Featured Slider', 'T4B Slider', 'add_users', __FILE__, 't4b_featured_plugin_menu', plugins_url('t4b-featured-slider/images/icon.png'));
+	add_submenu_page(__FILE__, __('Usage', t4b_DOMAIN ), __('Usage', t4b_DOMAIN ), 'add_users', __FILE__, 't4b_featured_plugin_menu');
+	add_submenu_page(__FILE__, 'Settings', 'Settings', 'manage_options', 't4b_settings', 't4b_settings_page');
+	add_action( 'admin_init', 'register_t4b_settings' );
+}
+
+function register_t4b_settings() {
+	register_setting( 't4b-settings-group', 't4b_option' );
+}
+
+include "admin/sidebar.php";
+
+function t4b_featured_plugin_menu() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	include "admin/usage.php";
+}
+function t4b_settings_page() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	include "admin/settings.php";
+}
+?>
