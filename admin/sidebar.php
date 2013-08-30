a818b2e45246d0c7bfdb06f250e0ff421549682e
moviehour <moviehour@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-30 09:40:46 +0000

    First version of T4B Featured Slider
    git-svn-id: https://plugins.svn.wordpress.org/t4b-featured-slider/trunk@764684 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -16,7 +16,7 @@
 		<div class="inside">
 			<p>Developer: <a href="http://facebook.com/IKIAlam">Iftekhar</a><br/>
 			E-Mail: moviehour@gmail.com<br/>
-			Website: <a href="http://www.tips4blog.com" target="_blank">www.tips4blog.com</a></p>
+			Website: <a href="http://www.tips4blog.com/" target="_blank">www.tips4blog.com</a></p>
 		</div>
 	</div>
 
@@ -26,9 +26,10 @@
 			<p>Price: Free!<br/>
 			Version: 1.0<br/>
 			Scripts: PHP + JS + CSS.<br/>
-			Requires: Wordpress 2.8+<br/>
-			First release: 28-Aug-2013<br/>
-			Published under: <a href="http://www.gnu.org/licenses/gpl.txt">GNU General Public License</a><br/></p>
+			Requires: Wordpress 3.0+<br/>
+			First release: 29-Aug-2013<br/>
+			Published under: <a href="http://www.gnu.org/licenses/gpl.txt">GNU General Public License</a><br/>
+			<a href="http://wordpress.org/plugins/t4b-featured-slider/faq/">FAQ</a> | <a href="http://wordpress.org/plugins/t4b-featured-slider/changelog/">Changelog</a><br/></p>
 		</div>
 	</div>
 </div>
