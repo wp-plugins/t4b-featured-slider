5b34ffa86bef7db57ea6710a749d141ee29507e7
moviehour <moviehour@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-21 15:15:41 +0000

    Update tags to 1.3
    git-svn-id: https://plugins.svn.wordpress.org/t4b-featured-slider/tags/1.3@862398 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/featured.php
@@ -0,0 +1,121 @@
+<div class="wrap">
+	<h2><img src="<?php echo WP_PLUGIN_URL; ?>/t4b-featured-slider/images/featured-slider.png" alt="T4B Featured Slider Lists"> T4B Featured Slider Lists</h2>
+	<?php
+	featured_post_install();
+	$msg = 'Please enter the URL of a post to get the ID';
+	if($_POST['get_id'] == 'Get ID') {
+		$url = $_POST['post_link'];
+		$postid = url_to_postid( $url );
+		$title = get_the_title(url_to_postid($url));
+		$postdata = get_post($postid, ARRAY_A);
+		$authorID = $postdata['post_author'];
+		$user_info = get_userdata($authorID);
+		$author_name = $user_info->user_login;
+		if($postid) {
+			$msg = '<b>The results are:</b> <br />
+					<b>URL of the post:</b> <a href="'.$url.'" target="_blank">'.$url.'</a><br />
+					<b>Post ID:</b> '.$postid.'<br />
+					<b>Post Title:</b> '.$title.'<br />
+					<b>Post Author Name:</b> '.$author_name.'<br />';
+		} else {	$msg = '<font color="red">Invalid URL!</font>'; }
+	} elseif ($_POST['list_id'] == 'Add Post') {
+		$pid = $_POST['postID'];
+		$postdata = get_post($pid, ARRAY_A);
+		$post_title = $postdata['post_title'];
+		$post_type = $postdata['post_type'];
+		if($post_type == 'post') { $qres = add_featured_post ($pid, $post_title); }
+		else { $qres = 'Sorry! The ID you have added is not a post!'; }
+	} elseif ($_POST['unlist_id'] == 'Delete') {
+		$pid = $_POST['rmvID'];
+		$dres = remove_featured_post ($pid);
+	}
+	?>
+	<?php if ($dres) { echo '<div id="message" class="updated"><p>'. $dres .'</p></div>'.PHP_EOL; } ?>
+	<?php if ($qres) { echo '<div id="message" class="updated"><p>'. $qres .'</p></div>'.PHP_EOL; } ?>
+	<div style="width: 68%; float: left;">
+		<form method="post" id="myForm" enctype="multipart/form-data">
+		<div class="postbox" style="display: block;float:left;margin:5px;clear:left; width: 99%;">
+			<h3 class="hndle" style="padding:5px; color:#007193;">Get the ID</h3>
+			<div class="inside">
+				<div><p align="justify">To get the ID of a post simply enter the URL of the post and then click on Get ID button:</p>
+   					<table class="form-table">
+       					<tr valign="top">
+       						<td>Enter URL:</td>
+       						<td width="50%"><input type="text" name="post_link" value="" size="60" /></td>
+							<td><input type="submit" name="get_id" class="button-primary" value="Get ID"></td>
+       					</tr>
+
+        				<tr valign="top"><td colspan="3"><?php echo $msg; ?></td></tr>
+   					</table>
+				</div>
+			</div>
+		</div>
+		<div class="postbox" style="display: block;float:left;margin:5px;clear:left; width: 99%;">
+			<h3 class="hndle" style="padding:5px; color:#007193;">Add Post in the Featured Lists</h3>
+			<div class="inside">
+				<div><p align="justify">To make a post as featured simply enter the post ID and click on Add ID button.</p>
+	   				<table class="form-table">
+       					<tr valign="top">
+   	   						<td width="30%">Enter the post ID:</td>
+    	    				<td width="20%"><input type="text" name="postID" size="10" value="" /></td>
+							<td><input type="submit" name="list_id" class="button-primary" value="Add Post" /></td>
+   	   					</tr>
+		    		</table>
+				</div>
+			</div>
+		</div>
+		<div class="postbox" style="display: block;float:left;margin:5px;clear:left; width: 99%;">
+			<h3 class="hndle" style="padding:5px; color:#007193;">Delete Post from the Featured Lists</h3>
+			<div class="inside">
+				<div><p align="justify">To delete a post simply enter the post ID and click on Delete button.</p>
+	   				<table class="form-table">
+       					<tr valign="top">
+   	   						<td width="30%">Enter the post ID:</td>
+    	    				<td width="70%"><input type="text" name="rmvID" size="10" value="" /></td>
+   	   					</tr>
+       					<tr valign="top">
+							<td><input type="submit" name="unlist_id" class="button-primary" value="Delete" /></td>
+   	   					</tr>
+		    		</table>
+				</div>
+			</div>
+		</div>
+		<div class="postbox" style="display: block;float:left;margin:5px;clear:left;width:99%; background:#F4F4F2;">
+			<h3 class="hndle" style="padding:5px; color:#007193;">Featured Post Lists</h3>
+			<div class="inside">
+				<div><p align="justify"><strong>Lists of all featured posts:</strong></p>
+   					<?php
+						$all_lists = get_featured_post_lists();
+						$option = count($all_lists);
+						$flists = '';
+						$sn = 1;
+						if(count($all_lists) > 0) {
+					?>
+							<table class="form-table" border="1" width="100%">
+	        					<thead><tr valign="top">
+									<td scope="row"><strong>SN</strong></td>
+									<td scope="row"><strong>ID</strong></td>
+       								<td scope="row"><strong>Post ID</strong></td>
+       								<td scope="row"><strong>Post Title</strong></td>
+	        						<td scope="row"><strong>Activity Date</strong></td>
+       							</tr></thead>
+								<tbody>
+								<?php foreach($all_lists as $flists) { ?>
+	        						<tr valign="top">
+										<td width="2%"><?php echo $sn; ?></td>
+										<td width="3%"><?php echo $flists->id; ?></td>
+										<td width="10%"><?php echo $flists->post_id; ?></td>
+										<td width="60%"><?php echo $flists->post_title; ?></td>
+										<td width="15%"><?php echo $flists->activity_date; ?></td>
+									</tr>
+								<?php $sn++; } ?>
+								</tbody>
+		   					</table>
+						<?php } else { echo "You didn't add any Featured Post yet!"; } ?>
+				</div>
+			</div>
+		</div>
+		</form>
+	</div>
+	<?php echo t4b_sidebar(); ?>
+</div>
--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,35 +1,18 @@
 <?php function t4b_sidebar() { ?>
 <div style="width: 30%; float: right;">
-
-	<div class="postbox" style="display: block; float: right; margin: 5px; clear: right; width: 99%;">
-		<div class="inside"><p><iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Ftips4blog&amp;width=310&amp;height=258&amp;colorscheme=light&amp;show_faces=true&amp;border_color=%23FAFAFA&amp;stream=false&amp;header=false&amp;appId=269102609767382" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:298px; height:258px;" ></iframe></p></div>
-	</div>
-
-	<div class="postbox" style="display: block; float: right; margin: 5px; clear: right; width: 99%;">
-		<h3 class="hndle" style="padding:5px; color:#007193;">Changes in Version 1.1</h3>
-		<div class="inside"><p align="justify">
-			We have upgraded "T4B Featured Slider" plugin from version 1.0 to version 1.1 with few changes:
-			<ol>
-				<li>You have to replace the code given in <strong>Usage</strong> section (Under "Code Usage Instruction") with the previous code.</li>
-				<li>Now you don't need to enter ID to delete a post from featured list. You will get a <strong>Delete</strong> button near each of the featured post. Just click on that button and you are done!</li>
-			</ol>
-		</p></div>
-	</div>
-
-	<div class="postbox" style="display: block; float: right; margin: 5px; clear: right; width: 99%;">
+	<div class="postbox" style="display: block; float: right; margin: 5px; clear: right; width: 97%;">
 		<h3 class="hndle" style="padding:5px; color:#007193;">Credits</h3>
 		<div class="inside">
 			<p>Developer: <a href="http://facebook.com/IKIAlam">Iftekhar</a><br/>
-			E-Mail: moviehour@gmail.com<br/>
 			Website: <a href="http://www.tips4blog.com" target="_blank">www.tips4blog.com</a></p>
 		</div>
 	</div>
 
-	<div class="postbox" style="display: block; float: right; margin: 5px; clear: right; width: 99%;">
+	<div class="postbox" style="display: block; float: right; margin: 5px; clear: right; width: 97%;">
 		<h3 class="hndle" style="padding:5px; color:#007193;">Plugin Info</h3>
 		<div class="inside">
 			<p>Price: Free!<br/>
-			Version: 1.1<br/>
+			Version: 1.3<br/>
 			Scripts: PHP + JS + CSS.<br/>
 			Requires: Wordpress 3.0+<br/>
 			First release: 29-Aug-2013<br/>
